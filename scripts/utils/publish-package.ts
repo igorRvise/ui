be3f5041de21ef29273b90b156eb3f250af48e2b
Igor Revenko <irevdev@gmail.com>
2023-08-02 11:10:16 +1000

    updated package access

--- scripts/utils/publish-package.ts
+++ scripts/utils/publish-package.ts
@@ -1,12 +1,13 @@
 import chalk from 'chalk'
 import execa from 'execa'
+
 import { Logger } from './Logger'
 
 const logger = new Logger('publish-package')
 
 export async function publishPackage({ path, name, tag }: { path: string; name: string; tag: string }) {
   try {
-    await execa('yarn', ['publish', path, '--tag', tag])
+    await execa('yarn', ['publish --access public', path, '--tag', tag])
     logger.success(`Package ${chalk.cyan(name)} was published`)
   } catch (error) {
     logger.error(`Failed to publish package ${chalk.red(name)}`)
